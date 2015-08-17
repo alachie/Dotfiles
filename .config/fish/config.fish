23a72e6ae90f44e64cec7f37a85fd5095d8874b5
alachie <lachie@alachie.com>
2015-08-17 17:41:57 +1000

    Added browser sync function

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -13,3 +13,8 @@ function hide
   defaults write com.apple.finder AppleShowAllFiles -bool false
   killall Finder
 end
+
+# start browser sync with custom arguments
+function bsync
+  browser-sync start --server --files \"*\" --no-ghost-mode
+end
