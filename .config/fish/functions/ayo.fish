fad9f28090363913a7061c262c082e765c40f702
alachie <lachie@alachie.com>
2015-08-21 15:40:27 +1000

    added bower.json script

--- .config/fish/functions/ayo.fish
+++ .config/fish/functions/ayo.fish
@@ -11,4 +11,42 @@ function ayo
   mkdir img
   mkdir includes
   mkdir svg
+
+  # create package.json
+  echo ' - creating package.json...'
+  echo '{
+    "name": "",
+    "version": "1.0.0",
+    "dependencies": {
+      "gulp": "^3.9.0",
+      "gulp-autoprefixer": "^2.3.1",
+      "gulp-cache": "^0.2.10",
+      "gulp-imagemin": "^2.3.0",
+      "gulp-include": "^2.0.2",
+      "gulp-jade": "^1.1.0",
+      "gulp-plumber": "^1.0.1",
+      "gulp-sass": "^2.0.4",
+      "gulp-uglify": "^1.2.0",
+      "gulp-watch": "^4.3.4"
+    }
+  }' > package.json
+
+  # create bower.json
+  echo ' - creating bower.json...'
+  echo '{
+    "name": "",
+    "version": "1.0.0",
+    "ignore": [
+      "**/.*",
+      "node_modules",
+      "bower_components",
+      "test",
+      "tests"
+    ],
+    "dependencies": {
+      "jquery": "~2.1.4",
+      "normalize.css": "~3.0.3"
+    }
+  }' > bower.json
+
 end
