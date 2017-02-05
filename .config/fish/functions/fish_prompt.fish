5370c684c775de26d00008a2b8e9ea1934f6fed4
alachie <_@lachie.co>
2017-02-05 18:59:24 +1100

    added emoji for up and down stream

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -13,10 +13,10 @@ function fish_prompt --description 'Write out the prompt'
 		set -g __fish_git_prompt_showupstream "informative"
 	end
 	if not set -q __fish_git_prompt_char_upstream_ahead
-		set -g __fish_git_prompt_char_upstream_ahead "↑"
+		set -g __fish_git_prompt_char_upstream_ahead "👆🏻 "
 	end
 	if not set -q __fish_git_prompt_char_upstream_behind
-		set -g __fish_git_prompt_char_upstream_behind "↓"
+		set -g __fish_git_prompt_char_upstream_behind "👇🏻 "
 	end
 	if not set -q __fish_git_prompt_char_upstream_prefix
 		set -g __fish_git_prompt_char_upstream_prefix ""
